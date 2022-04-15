9bff305ef68406043469753e5672922d1f7fef35
Matheus Paixão <mpgs@cin.ufpe.br>
2022-04-14 22:28:45 -0300

    Adding functions cast, trace and plane intersection

---
+++ object.py
@@ -0,0 +1,67 @@
+from logging import exception
+import numpy as np
+import enum
+import math
+
+
+class ObjectType(enum.Enum):
+    SPHERE = 1
+    PLANE = 2
+
+
+class Object:
+    ''''Represents an object on the scene'''
+    e = 10 ^ (-6)  # constante para evitar erros da aritmetica de ponto flutuante
+
+    def __init__(self, Cr, Cg, Cb, Px, Py, Pz, Nx, Ny, Nz) -> None:
+        self.color = np.array([Cr, Cg, Cb])
+        self.sample_point = np.array([Px, Py, Pz])
+        self.normal_vector = np.array([Nx, Ny, Nz])
+        self.type = ObjectType.PLANE
+
+    # @classmethod
+    # def sphere_constructor(Ox, Oy, Oz, r):
+    #   self.
+
+    # def __init__(self, Cr, Cg, Cb, Ox, Oy, Oz, r) -> None:
+    #     self.color = np.array([Cr, Cg, Cb])
+    #     self.center = np.array([Ox, Oy, Oz])
+    #     self.ray = r
+    #     self.type = ObjectType.SPHERE
+
+    def intersection(self, ray_origin, ray_dir):
+        if self.type == ObjectType.SPHERE:
+            return self._intersect_sphere(ray_origin, ray_dir)
+        else:
+            return self._intersect_plane(ray_origin, ray_dir)
+
+    # TODO
+    def _intersect_sphere(self, ray_origin, ray_dir):
+        t = math.inf
+        return t
+
+    def _intersect_plane(self, ray_origin, ray_dir):
+        print(ray_origin)
+        print(ray_dir)
+        print(self.normal_vector)
+        t = math.inf
+        den = np.inner(ray_dir, self.normal_vector)
+        if abs(den) > Object.e:
+            t = np.inner((self.sample_point - ray_origin),
+                         self.normal_vector)/den
+            if t < 0:
+                raise exception("Ray don't intersect the plane!")
+            else:
+                return t
+        else:
+            raise exception(
+                "Inner product between ray_dir and normal_vector too low")
+
+
+if __name__ == "__main__":
+    ray_dir = np.array([-0.71, 0.71, 0])
+    plane = Object(255, 255, 255, 10, 10, 10, -2, -2, 2)
+    origin = np.array([2, 2, 2])
+
+    t = plane.intersection(origin, ray_dir)
+    print(t)
--- projeto-pg.py
+++ projeto-pg.py
@@ -1,14 +1,17 @@
 import numpy as np
 
+from object import Object
+
 # INPUT:
 # v_res h_res                      //resolucao vertical e horizontal
 # s d                              //s=largura do pixel |  d= distancia do centro da camera ao plano de exibicao
-# Ex Ey Ez                         // origem do sistema de coordenadas e dos raios primarios (vulgo camera),
+# Ex Ey Ez                         // origem do sistema de coordenadas e dos raios primarios
 # Lx Ly Lz                         // para onde a camera aponta
 # upx upy upz                      // vetor que aponta pra cima
-# k_obj                             // qt de objetos
-# C.r C.g C.b * Ox Oy Oz r          // circulo
-# C.r C.g C.b / Px Py Pz nx ny nz   //plano
+# B.r B.g B.b                      // Cor do Background
+#  k_obj                           // qt de objetos
+# C.r C.g C.b * Ox Oy Oz r         // circulo
+# C.r C.g C.b / Px Py Pz nx ny nz  //plano
 
 # Variaveis da Tela
 v_res = 4  # Altura vertical da tela
@@ -17,12 +20,14 @@ s = 10       # Tamanho de um pixel
 d = 5        # Distancia da camera ao plano de exibicao
 
 # Variaveis da camera
+
 # origem do sistema de coordenadas e dos raios primarios (vulgo camera),
 E = np.array([2, 2, 2])
-L = np.array([0, 0, 1])  # para onde a camera aponta
+L = np.array([4.67, 4.67, -0.67])  # para onde a camera aponta
 up = np.array([0, 0, 1])  # vetor que aponta pra cima
 
 objects = []  # Array de objetos
+background_color = np.array([255, 0, 0])  # Cor do plano de fundo (RGB)
 
 
 def render(v_res, h_res, s, d, E, L, up):
@@ -37,19 +42,43 @@ def render(v_res, h_res, s, d, E, L, up):
     # Lancamento dos raios
     q_00 = E - d * w + s * (((v_res - 1)/2)*v - ((h_res - 1)/2)*u)
     print(q_00)
-    for i in range(v_res):
-        for j in range(h_res):
+    for i in image:
+        for j in i:
             q_ij = q_00 + s * (j * u - i * v)
             dir_ray = (q_ij-E)/np.linalg.norm(q_ij - E)
-            image[i][j] = cast(E, dir_ray)
-            print(q_ij, end=" ")
+            j = cast(E, dir_ray)
+            # print(q_ij, end=" ")
         print()
     return image
 
+# TODO: testar depois de implementar o trace
+
 
 def cast(E, dir_ray):
-    return
+    c = background_color
+    S = trace(E, dir_ray)
+
+    if (S):
+        t, closest = min(S)
+        c = closest.color
+    return c
+
+
+objects.append(Object(255, 255, 255, 10, 10, 10, -2, -2, 2))
+# TODO: testar depois de implementar os intersects
+
+
+def trace(E, dir_ray):
+    s = []
+    for obj in objects:
+        try:
+            t = obj.intersection(E, dir_ray)
+            s.append((t, obj))
+        except:
+            pass
+    return s
 
 
 # MAIN
-render(v_res, h_res, s, d, E, L, up)
+if __name__ == "__main__":
+    render(v_res, h_res, s, d, E, L, up)
