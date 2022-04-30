1e3aef2ecd0bbd57496633a258338e60eacb3b14
Matheus Paixão <mpgs@cin.ufpe.br>
2022-04-30 20:54:58 -0300

    Bug fixes

--- img_builder.py
+++ img_builder.py
@@ -1,20 +1,26 @@
-import numpy as np 
+import numpy as np
 import math
 import json
 import matplotlib.pyplot as plt
 from light_src import light_src
 
-from object import Plane,Sphere
+from object import Plane, Sphere
 
 objects = []  # Array de objetos
 background_color = np.array([255, 0, 0])  # Cor do plano de fundo (RGB)
 ambient_light = np.zeros(3)
 lights = []
-e = 10**(-5) # Constant to prevent shadow acne
+e = 10**(-5)  # Constant to prevent shadow acne
+
 
 def reflect(dir_light, surface_normal):
     # Returns the reflected light vector
-    return 2 * np.inner(dir_light,surface_normal)*surface_normal-dir_light
+    return 2 * np.inner(surface_normal, dir_light)*surface_normal - dir_light
+
+
+def tuple_comparator(object):
+    return object[0]
+
 
 def shade(obj, intersection_point, dir_focus, surface_normal):
     obj_color = obj.color
@@ -22,35 +28,41 @@ def shade(obj, intersection_point, dir_focus, surface_normal):
     obj_kd = obj.k_d
     obj_ks = obj.k_s
     obj_exp = obj.exp
-    
-    point_color = obj_ka*obj_color*ambient_light # Calculates the ambient color influence
+
+    # Calculates the ambient color influence
+    point_color = obj_ka*obj_color*ambient_light
 
     for light in lights:
         point_to_light_vec = light.position-intersection_point
         light_dir = (point_to_light_vec)/np.linalg.norm(point_to_light_vec)
-        
-        reflected = reflect(light)
-        intersection_point_corrected = intersection_point - e*light_dir
+
+        reflected = reflect(light_dir, surface_normal)
+        intersection_point_corrected = intersection_point + e*light_dir
         intersected_objs = trace(intersection_point_corrected, light_dir)
-        
+
+        # closest_obj = sorted(
+        #     intersected_objs, key=lambda obj: tuple_comparator(obj))[0]
         closest_t = math.inf
-        
+
         if (intersected_objs):
             for t_obj in intersected_objs:
                 if t_obj[0] < closest_t:
                     closest_t = t_obj[0]
-                    
-        if (intersected_objs or 
-            np.inner(light_dir, (point_to_light_vec - intersection_point_corrected)) < closest_t):
+
+        if (not intersected_objs or
+                np.inner(light_dir, (light.position - intersection_point_corrected)) < closest_t):
             if (np.inner(surface_normal, light_dir) > 0):
-                point_color = point_color + obj_kd*obj_color * (np.inner(surface_normal, light_dir))*light.intensity
+                point_color = point_color + obj_kd*obj_color * \
+                    (np.inner(surface_normal, light_dir))*light.intensity
             if (np.inner(dir_focus, reflected) > 0):
-                point_color = point_color + (obj_ks*(np.inner(dir_focus, reflected))**obj_exp)*light.intensity
-        
+                point_color = point_color + \
+                    (obj_ks*((np.inner(dir_focus, reflected))**obj_exp))*light.intensity
+
     return point_color
 
+
 def render(v_res, h_res, tamanho_px, dist_focal, foco_camera, mira_camera, up):
-    image = np.zeros((v_res, h_res,3),dtype=np.uint8)
+    image = np.zeros((v_res, h_res, 3), dtype=np.uint8)
 
     # Criacao do sistema de coordenadas da camera {u,v,w}
     w = (foco_camera - mira_camera) / np.linalg.norm(foco_camera - mira_camera)
@@ -59,12 +71,14 @@ def render(v_res, h_res, tamanho_px, dist_focal, foco_camera, mira_camera, up):
     v = np.cross(w, u)
 
     # Lancamento dos raios
-    q_00 = foco_camera - dist_focal * w + tamanho_px * (((v_res - 1)/2)*v - ((h_res - 1)/2)*u)
+    q_00 = foco_camera - dist_focal * w + tamanho_px * \
+        (((v_res - 1)/2)*v - ((h_res - 1)/2)*u)
     for i in range(v_res):
         for j in range(h_res):
             q_ij = q_00 + tamanho_px * (j * u - i * v)
             dir_ray = (q_ij-foco_camera)/np.linalg.norm(q_ij - foco_camera)
-            image[i][j] = cast(foco_camera, dir_ray)
+            image[i][j] = cast(foco_camera, dir_ray) * \
+                np.array([255, 255, 255])
     return image
 
 
@@ -75,29 +89,30 @@ def cast(foco_camera, dir_ray):
     if (intersected_objs):
         closest_t = math.inf
         closest_obj = None
-        
+
         for t_obj in intersected_objs:
             if t_obj[0] < closest_t:
                 closest_t = t_obj[0]
                 closest_obj = t_obj[1]
                 #cor_background = closest_obj.color
-        
+
         intersection_point = foco_camera + dir_ray*closest_t
-        point_color = shade(closest_obj, intersection_point, -dir_ray, closest_obj.get_normal(intersection_point))
+        point_color = shade(closest_obj, intersection_point, -
+                            dir_ray, closest_obj.get_normal(intersection_point))
     return point_color
 
 
 def trace(foco_camera, dir_ray):
-    tamanho_px = []
+    s = []
     for obj in objects:
         t = obj.intersect(foco_camera, dir_ray)
         if t:
-            tamanho_px.append((t, obj))
-    return tamanho_px
+            s.append((t, obj))
+    return s
 
 
 def run_by_json(path):
-    # Carrega o json em specs 
+    # Carrega o json em specs
     file = open(path)
     specs = json.load(file)
     file.close()
@@ -110,33 +125,37 @@ def run_by_json(path):
     objects_json = specs["objects"]
 
     for obj in objects_json:
-        r,g,b = obj["color"]
+        r, g, b = obj["color"]
+        r, g, b = r/255, g/255, b/255
         k_a = obj["ka"]
         k_d = obj["kd"]
         k_s = obj["ks"]
         exp = obj["exp"]
-        
-        if obj.get("plane"): # Se o objeto for um plano ...
-            plane = obj["plane"]
-            sx,sy,sz = plane["sample"]
-            nx,ny,nz = plane["normal"]
 
-            objects.append(Plane(r,g,b,sx,sy,sz,nx,ny,nz,k_a,k_d,k_s,exp))
+        if obj.get("plane"):  # Se o objeto for um plano ...
+            plane = obj["plane"]
+            sx, sy, sz = plane["sample"]
+            nx, ny, nz = plane["normal"]
+
+            objects.append(Plane(r, g, b, sx, sy, sz, nx,
+                           ny, nz, k_a, k_d, k_s, exp))
 
         elif obj.get("sphere"):
             sphere = obj["sphere"]
-            cx,cy,cz = sphere["center"]
+            cx, cy, cz = sphere["center"]
             radius = sphere["radius"]
 
-            objects.append(Sphere(r,g,b,cx,cy,cz,radius,k_a,k_d,k_s,exp))
+            objects.append(Sphere(r, g, b, cx, cy, cz,
+                           radius, k_a, k_d, k_s, exp))
 
     # Define a luz ambiental
     global ambient_light
-    ambient_light = np.array(specs["ambient_light"])
+    ambient_light = np.array(specs["ambient_light"])/255
 
     # Pega a lista de fontes de luz
     for l in specs["lights"]:
-        light = light_src(np.array(l["position"]), np.array(l["intensity"]))
+        light = light_src(np.array(l["position"]),
+                          np.array(l["intensity"])/255)
         lights.append(light)
 
     # Gera a imagem
--- object.py
+++ object.py
@@ -4,9 +4,11 @@ import enum
 import math
 import abc
 
+
 class ObjectType(enum.Enum):
     SPHERE = 1
     PLANE = 2
+
 
 class Object:
     ''''Represents an object on the scene'''
@@ -28,6 +30,7 @@ class Object:
     def get_normal(self, point_on_object):
         pass
 
+
 class Plane(Object):
     def __init__(self, Color_r, Color_g, Color_b, Px, Py, Pz, Nx, Ny, Nz, K_a, K_d, K_s, exp):
         '''Create a plane receiving its color, a sample point and its normal vector'''
@@ -36,15 +39,14 @@ class Plane(Object):
         self.normal_vector = np.array([Nx, Ny, Nz])
         self.type = ObjectType.PLANE
 
-
     def __str__(self):
         text = ""
-        text += "{" + str(self.type.name) +":"               + "\n"
+        text += "{" + str(self.type.name) + ":" + "\n"
 
-        text += "Sample Point: " + str(self.sample_point)    + "\n"
-        text += "Normal: " + str(self.normal_vector)         + "\n"
-        text += "Color: " + str(self.color) + "}"            + "\n"
-        
+        text += "Sample Point: " + str(self.sample_point) + "\n"
+        text += "Normal: " + str(self.normal_vector) + "\n"
+        text += "Color: " + str(self.color) + "}" + "\n"
+
         return text
 
     def intersect(self, ray_origin, ray_dir):
@@ -59,15 +61,16 @@ class Plane(Object):
             else:
                 return t
         else:
-            #Inner product between ray_dir and normal_vector too low
+            # Inner product between ray_dir and normal_vector too low
             return
 
     def get_normal(self, point_on_object=None):
         '''Returns the normalized norm of the plane'''
-        norm =  np.linalg.norm(self.normal_vector)
+        norm = np.linalg.norm(self.normal_vector)
         return self.normal_vector/norm
 
-class Sphere(Object):       
+
+class Sphere(Object):
     def __init__(self, Color_r, Color_g, Color_b, Ox, Oy, Oz, r, K_a, K_d, K_s, exp):
         '''Create a sphere receiving its color and center'''
         super().__init__(Color_r, Color_g, Color_b, K_a, K_d, K_s, exp)
@@ -77,10 +80,10 @@ class Sphere(Object):
 
     def __str__(self):
         text = ""
-        text += "{" + str(self.type.name) +":"    + "\n"
+        text += "{" + str(self.type.name) + ":" + "\n"
 
-        text += "Center: " + str(self.center)     + "\n"
-        text += "radius: " + str(self.radius)     + "\n"
+        text += "Center: " + str(self.center) + "\n"
+        text += "radius: " + str(self.radius) + "\n"
         text += "Color: " + str(self.color) + "}" + "\n"
 
         return text
@@ -90,14 +93,14 @@ class Sphere(Object):
         t_ca = np.inner(l, ray_dir)
         d_2 = (np.inner(l, l)) - (t_ca)*(t_ca)
         if d_2 > (self.radius)**2:
-            #Ray doesn't intersect the sphere!"
+            # Ray doesn't intersect the sphere!"
             return
         else:
             t_hc = math.sqrt(self.radius**2 - d_2)
             t0 = t_ca - t_hc
             t1 = t_ca + t_hc
             if t0 > t1:
-                t0,t1 = t1,t0
+                t0, t1 = t1, t0
             if t0 < 0:
                 if t1 < 0:
                     # Off-sColor_reen intersection with sphere
